2c2ca790d2a3e9ef6eff3ec78f75f953df70d162
Александр Шилов <aleksandr.schilow2012@gmail.com>
2018-05-16 21:53:00 +0300

    Add hello world example

---
+++ examples/src/main/kotlin/com/github/ashlanderr/kotlin/ui/helloworld/HelloWorld.kt
@@ -0,0 +1,20 @@
+package org.github.ashlanderr.kotlin.ui.helloworld
+
+import com.github.ashlanderr.kotlin.ui.core.Application
+import com.github.ashlanderr.kotlin.ui.layout.center
+import com.github.ashlanderr.kotlin.ui.text.textBlock
+
+class HelloWorldApp : Application() {
+    override fun render() = center {
+        child = textBlock {
+            text = "Hello, World!"
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    val app = HelloWorldApp()
+    //app.isUndecorated = true
+    app.setBounds(200, 200, 800, 600)
+    app.isVisible = true
+}
