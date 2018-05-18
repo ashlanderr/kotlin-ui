5b69d389a40396fdc3c8ef10c79414cfc194b0b0
Александр Шилов <aleksandr.schilow2012@gmail.com>
2018-05-18 18:25:45 +0300

    Add copy method for Constraint

--- ui/src/main/kotlin/com/github/ashlanderr/kotlin/ui/core/Constraint.kt
+++ ui/src/main/kotlin/com/github/ashlanderr/kotlin/ui/core/Constraint.kt
@@ -3,9 +3,11 @@ package com.github.ashlanderr.kotlin.ui.core
 interface Constraint {
     val size: Double
     fun compute(min: Double, max: Double): Double
+    fun copy(size: Double): Constraint
 
     class Min(override val size: Double) : Constraint {
         override fun compute(min: Double, max: Double) = min
+        override fun copy(size: Double) = Min(size)
 
         override fun toString(): String {
             return "Constraint.Min(size=$size)"
@@ -14,6 +16,7 @@ interface Constraint {
 
     class Max(override val size: Double) : Constraint {
         override fun compute(min: Double, max: Double) = max
+        override fun copy(size: Double) = Max(size)
 
         override fun toString(): String {
             return "Constraint.Max(size=$size)"
