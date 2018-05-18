6f65ab48525d8b114eabab75a168cc4d5f034b0c
Александр Шилов <aleksandr.schilow2012@gmail.com>
2018-05-18 18:26:18 +0300

    Fix window closing without exiting program

--- examples/src/main/kotlin/com/github/ashlanderr/kotlin/ui/helloworld/HelloWorld.kt
+++ examples/src/main/kotlin/com/github/ashlanderr/kotlin/ui/helloworld/HelloWorld.kt
@@ -3,6 +3,7 @@ package com.github.ashlanderr.kotlin.ui.helloworld
 import com.github.ashlanderr.kotlin.ui.core.Application
 import com.github.ashlanderr.kotlin.ui.layout.center
 import com.github.ashlanderr.kotlin.ui.text.textBlock
+import javax.swing.JFrame
 
 class HelloWorldApp : Application() {
     override fun render() = center {
@@ -16,5 +17,6 @@ fun main(args: Array<String>) {
     val app = HelloWorldApp()
     //app.isUndecorated = true
     app.setBounds(200, 200, 800, 600)
+    app.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
     app.isVisible = true
 }
