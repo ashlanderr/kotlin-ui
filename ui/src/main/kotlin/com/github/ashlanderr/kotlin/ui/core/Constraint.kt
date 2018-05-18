a9b95bcee1f44950c296aca4d8c64774ae6d9657
Александр Шилов <aleksandr.schilow2012@gmail.com>
2018-05-18 10:14:27 +0300

    Add toString functions for Constraint

--- ui/src/main/kotlin/com/github/ashlanderr/kotlin/ui/core/Constraint.kt
+++ ui/src/main/kotlin/com/github/ashlanderr/kotlin/ui/core/Constraint.kt
@@ -6,9 +6,17 @@ interface Constraint {
 
     class Min(override val size: Double) : Constraint {
         override fun compute(min: Double, max: Double) = min
+
+        override fun toString(): String {
+            return "Constraint.Min(size=$size)"
+        }
     }
 
     class Max(override val size: Double) : Constraint {
         override fun compute(min: Double, max: Double) = max
+
+        override fun toString(): String {
+            return "Constraint.Max(size=$size)"
+        }
     }
 }
