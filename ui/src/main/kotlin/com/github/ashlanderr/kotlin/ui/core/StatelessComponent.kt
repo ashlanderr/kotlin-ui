6f3b3c9cf21fd4467c277eb008af750cc225bf9e
Александр Шилов <aleksandr.schilow2012@gmail.com>
2018-05-20 10:54:03 +0300

    Add stateless component

---
+++ ui/src/main/kotlin/com/github/ashlanderr/kotlin/ui/core/StatelessComponent.kt
@@ -0,0 +1,49 @@
+package com.github.ashlanderr.kotlin.ui.core
+
+import java.awt.Graphics2D
+
+abstract class StatelessComponent(final override var key: Any? = null) : Node {
+    override val renderLeft get() = child.renderLeft
+    override val renderTop get() = child.renderTop
+    override val renderWidth get() = child.renderWidth
+    override val renderHeight get() = child.renderHeight
+
+    final override var parent: Node? = null
+        private set
+
+    private var child: Node = EmptyNode
+
+    override fun measure(g: Graphics2D, w: Constraint, h: Constraint) {
+        renderAndMerge()
+        child.measure(g, w, h)
+    }
+
+    override fun arrange(left: Double, top: Double) {
+        child.arrange(left, top)
+    }
+
+    override fun render(g: Graphics2D) {
+        child.render(g)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun mount(parent: Node?) {
+        this.parent = parent
+        renderAndMerge()
+    }
+
+    override fun unmount() {
+        child = MergeProcessor.merge(child, EmptyNode)
+    }
+
+    override fun childAtPoint(point: Point): Node? {
+        return child.childAtPoint(point)
+    }
+
+    abstract fun render(): Node
+
+    private fun renderAndMerge() {
+        val newChild = render()
+        child = MergeProcessor.merge(child, newChild, this)
+    }
+}
