c4689a0366234a9e2c97a88b012d93b2a5447732
Александр Шилов <aleksandr.schilow2012@gmail.com>
2018-05-25 10:10:16 +0300

    Add more icons

--- ui/src/main/kotlin/com/github/ashlanderr/kotlin/ui/text/Icons.kt
+++ ui/src/main/kotlin/com/github/ashlanderr/kotlin/ui/text/Icons.kt
@@ -2,7 +2,13 @@ package com.github.ashlanderr.kotlin.ui.text
 
 object Icons {
     val TIMES = IconData(0xf00d, Fonts.AWESOME_SOLID)
+    val ARROW_LEFT = IconData(0xf060, Fonts.AWESOME_SOLID)
     val EXPAND = IconData(0xf065, Fonts.AWESOME_SOLID)
     val COMPRESS = IconData(0xf066, Fonts.AWESOME_SOLID)
+    val PLUS = IconData(0xf067, Fonts.AWESOME_SOLID)
     val MINUS = IconData(0xf068, Fonts.AWESOME_SOLID)
+    val PERCENT = IconData(0xf295, Fonts.AWESOME_SOLID)
+    val DIVIDE = IconData(0xf529, Fonts.AWESOME_SOLID)
+    val EQUALS = IconData(0xf52c, Fonts.AWESOME_SOLID)
+    val PERCENTAGE = IconData(0xf541, Fonts.AWESOME_SOLID)
 }
